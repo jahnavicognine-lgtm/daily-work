64fd68d43804e4611bebfcc4c9bddc1f82b26e3d
gitjahnavi <jahnavitenneti@gmail.com>
2025-08-17 09:08:29 +0530

    Day 4 work

---
+++ day4/Constructor/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Constructor
+{
+    internal class Program
+    {
+        public int x;
+        public int y=10;
+        public Program()
+        {
+            x=10;
+            Console.WriteLine("Non  static constructor called");
+        }
+        static Program() {
+            //cannot declare the variables except local variables here because static variables  are declared at runtime
+            Console.WriteLine("Static constructor is called");
+        }
+        static void Main(string[] args)
+        {
+            
+            Program obj = new Program();
+            Console.WriteLine("The value of X:" +obj.x +"\tand Y:"+obj.y);
+        }
+    }
+}
---
+++ day4/StaticClassMethodVariables/Program.cs
@@ -0,0 +1,58 @@
+﻿//static class
+/*using System.Data;
+
+namespace StaticClassMethodVariables
+{
+    internal class Program
+    {
+        class Class1
+        {
+           
+           public  static int b = 10;//no need of using obj
+           public int c = 20;
+           public  int d;
+        }
+        public static class Class2//only static members are allowed into static class
+        {
+      
+           public static int f = 89;
+         
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Class1 obj = new Class1();
+           
+            Console.WriteLine(Class1.b);
+            Console.WriteLine(obj.c);
+            Console.WriteLine(obj.d);
+           
+            Console.WriteLine(Class2.f);
+            
+            
+        }
+    }
+}
+*/
+
+namespace StaticClassMethodVariables
+{
+    internal class Program
+    {
+        public static string Method()
+        {
+            Console.WriteLine("Static Method");
+            return "hello";
+        }
+        public void Method1()
+        {
+            Console.WriteLine("Non Static Method");
+        }
+        public static void Main(string[] args) {
+            Program obj = new Program();
+            obj.Method1();
+            Method();
+        }
+    }
+}
